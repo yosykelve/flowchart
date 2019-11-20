0ec73299072c4aeffd47d40343e1b67c0ec3e6dc
yoshikawayuu <yoshikawayuu@192.168.0.24>
2019-11-20 16:19:34 +0900

    課題です

--- flowchart/src/lesson3/No24.java
+++ flowchart/src/lesson3/No24.java
@@ -5,7 +5,7 @@ public class No24 {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		double yokin = 100000;
-		for( nensu = 1; nensu <= 5; nensu++) {
+		for(Integer nensu = 1; nensu <= 5; nensu++) {
 			yokin = yokin * 1.1;
 		}
 	}
